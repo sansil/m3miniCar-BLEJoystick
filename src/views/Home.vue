eb79fa1ae1da809fb37acd59a29c028789335aaa
sansil <sansil32@gmail.com>
2019-04-25 16:15:17 -0300

    commit inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a class="button is-info" @click="searchBLE">BLE!</a>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+require("../assets/sass/main.scss");
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  components: {},
+  data() {
+    return {
+      configServiceUuid: "0000f00d-1212-efde-1523-785fef13d123"
+    };
+  },
+  mounted() {},
+  methods: {
+    searchBLE() {
+      console.log(navigator);
+      navigator.bluetooth
+        .requestDevice({
+          acceptAllDevices: true,
+          optionalServices: [this.configServiceUuid]
+        })
+        .then(device => {
+          console.log(device);
+        })
+        .catch(error => {
+          console.log("Argh! " + error);
+        });
+    }
   }
-}
+};
 </script>
