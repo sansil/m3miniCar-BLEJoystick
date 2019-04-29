a674bfad1d3b69b55281a910b10e411f6894a4b9
sansil <sansil32@gmail.com>
2019-04-29 15:57:25 -0300

    prototipo terminado, escribe ambas  char

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <a class="button is-info" @click="searchBLE">BLE!</a>
+    <joystick></joystick>
   </div>
 </template>
 
@@ -9,35 +8,15 @@
 // @ is an alias to /src
 
 require("../assets/sass/main.scss");
+import Joystick from "../components/Joystick";
 
 export default {
   name: "home",
-  components: {},
+  components: { Joystick },
   data() {
-    return {
-      configServiceUuid: "0000f00d-1212-efde-1523-785fef13d123"
-    };
+    return {};
   },
   mounted() {},
-  methods: {
-    async searchBLE() {
-      try {
-        console.log("Requesting any Bluetooth Device...");
-        const device = await navigator.bluetooth.requestDevice({
-          // filters: [...] <- Prefer filters to save energy & show relevant devices.
-          acceptAllDevices: true,
-          optionalServices: [this.configServiceUuid]
-        });
-        console.log("Connecting to GATT Server...");
-        const server = await device.gatt.connect();
-
-        console.log("Getting Service...");
-        const service = await server.getPrimaryService(this.configServiceUuid);
-      } catch (error) {
-        document.querySelector("#writeButton").disabled = true;
-        console.log("Argh! " + error);
-      }
-    }
-  }
+  methods: {}
 };
 </script>
