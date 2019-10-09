367232ff5f20127c4264d0224a9ad7be49ebb97d
sansil <sansil32@gmail.com>
2019-10-09 20:15:18 -0300

    deploy

---
+++ precache-manifest.20d2fe0c2465e793c0b5d2151c950c4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/m30Joystick/robots.txt"
+  },
+  {
+    "revision": "9237f6e28ad4a3dd81ca",
+    "url": "/m30Joystick/js/chunk-vendors.cddf14b6.js"
+  },
+  {
+    "revision": "308cd2dfb3545fac0efb",
+    "url": "/m30Joystick/js/app.edeefe79.js"
+  },
+  {
+    "revision": "d7c8e80db09af3673212ecc57dd319ae",
+    "url": "/m30Joystick/index.html"
+  },
+  {
+    "revision": "943bce36da76d340c6c94aaf2f9dd6c5",
+    "url": "/m30Joystick/img/f1.943bce36.jpg"
+  },
+  {
+    "revision": "308cd2dfb3545fac0efb",
+    "url": "/m30Joystick/css/app.0a2a12ab.css"
+  }
+];
